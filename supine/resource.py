258a9e79d934186e839d9ba74f6556dab8fb06c6
Steven James <sjames@jacksonfurnind.com>
2023-02-22 16:57:40 -0500

    refactor Resource to not use dataclass (too much magic, not enough documentation)

--- supine/resource.py
+++ supine/resource.py
@@ -1,7 +1,6 @@
-import dataclasses
 import warnings
 from functools import cached_property
-from typing import Callable, List, Type, Union
+from typing import List, Type, Union
 
 from pydantic import BaseModel, create_model
 
@@ -11,35 +10,68 @@ from supine.filter import Filter
 _resource_registry = {}
 
 
-class ResourceMeta(type):
-    def __new__(cls, cls_name, bases, attrs):
-        return cls
-
-
-@dataclasses.dataclass(kw_only=True)
 class Resource:
-    singular_name: str
-    plural_name: str
+    def __init__(
+        self,
+        *,
+        singular_name: str,  # string name of a single instance of this Resource (example: 'territory')
+        plural_name: str,  # string name of plural instances of this Resource (example: 'territories')
+        orm_class: Type,  # orm base model
+        model: Type[BaseModel],  # a fastapi response_model for a single response
+        create_params: Type[BaseModel] = None,  # for create/POST parameters
+        update_params: Type[BaseModel] = None,  # for update/PATCH parameters
+        query_filter: Union[Type[Filter], None] = None,
+        expansions: List[Union["Resource", str]] = None,
+        etag_attr: str = None,
+        last_modified_attr: str = None,
+        max_age: int = 0,  # max cache age in seconds
+    ):
+        """
+        A Resource ties together orm, de/serialization, and parameter models.
+        SupineRouter can use this resource to generate CRUD routes
 
-    orm_class: Type  # orm base model
-    model: Type[BaseModel]  # a fastapi response_model for a single response
-    create_params: Type[BaseModel] = None  # a pydantic model for create/POST parameters
-    update_params: Type[
-        BaseModel
-    ] = None  # a pydantic model for update/PATCH parameters
+        :param singular_name: example 'territory', used to generate url paths and messages for this resource
+            will be registered in a dict, so must be unique among all Resources in this project
 
-    query_filter: Union[
-        Type[Filter], Callable[[], None]
-    ] = lambda: None  # lambda: None for no filter
-    expansions: List[Union["Resource", str]] = dataclasses.field(
-        default_factory=list
-    )  # a list of Resources this Resource may reference
+        :param plural_name: example 'territories' used to generate messages and
+            to access relationships to this resource
 
-    etag_attr: str = None
-    last_modified_attr: str = None
-    max_age: int = 0  # max cache age in seconds
+        :param orm_class: the SQLAlchemy database model for this resource
 
-    def __post_init__(self):
+        :param model: the pydantic serialization model for this resource (pydantic.parse_obj_as(model, orm_class())
+
+        :param create_params: pydantic model to create an orm_class instance as in orm_class(**params)
+
+        :param update_params: pydantic model to update an orm_class instance as in setattr(instance, param, val)
+
+        :param query_filter: Filter model -- dependency injected for getting a list of this resource, then
+          used to filter the sqlalchemy query and/or query results
+
+        :param expansions: list of other Resources accessible on orm_class instances as getattr(instance, plural_name)
+          can be provided as strings corresponding to the singular_name to avoid circular reference problems
+
+        :param etag_attr: str name of an attribute on orm_class that returns an etag (example a rowversion()
+            from SQL Server or a uuid on). Used to determine if client-side cache is fresh. Only get-by-key
+            routes allow caching.
+
+        :param last_modified_attr: str name of an attribute on orm_class that returns a datetime representing
+            its last update time. Used to determine if client-side cache is fresh. Only get-by-key
+            routes allow caching.
+
+        :param max_age: number of seconds to indicate to client as the maximum amount of time to
+            cache this resource. Only get-by-key routes allow caching.
+        """
+        self.singular_name = singular_name
+        self.plural_name = plural_name
+        self.orm_class = orm_class
+        self.model = model
+        self.create_params = create_params
+        self.update_params = update_params
+        self.query_filter = query_filter or null_filter
+        self.expansions = expansions or []
+        self.etag_attr = etag_attr
+        self.last_modified_attr = last_modified_attr
+        self.max_age = max_age
         self.register()
 
     def register(self):
@@ -79,3 +111,8 @@ class Resource:
             exp if isinstance(exp, Resource) else _resource_registry[exp]
             for exp in self.expansions
         ]
+
+
+def null_filter():
+    """Used where a Filter is expected but no filter is necessary"""
+    return None
